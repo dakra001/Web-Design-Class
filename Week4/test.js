3de57a2405e8ad4fb1532eaba4b64fa4387a40fc
dakra001 <dana.treml@gmail.com>
2025-03-24 18:34:48 -0500

    First Website Upload

---
+++ Week4/test.js
@@ -0,0 +1,10 @@
+// var userName = prompt("What is your name?"
+var newHeaderText = prompt("What would you like the header to say?")
+var header =document.getElementById("testMenu")
+header.innerHTML = newHeaderText;
+
+var Coffeeprice = prompt("What would you like the price of coffee to be?")
+var newcoffeeprice = document.getElementById("Coffeeprice")
+newcoffeeprice.innerHTML = Coffeeprice;
+
+// alert(header.innerHTML)
